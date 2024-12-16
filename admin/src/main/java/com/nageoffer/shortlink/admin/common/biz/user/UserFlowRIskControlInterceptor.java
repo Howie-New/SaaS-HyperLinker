6b433a01841c8dbf94af8bf11c1d879d338b16cb
Howie New <niujiahao1942@outlook.com>
2024-12-16 15:29:37 +0800

    refactor: 登陆和限流从Filter改为Interceptor

--- admin/src/main/java/com/nageoffer/shortlink/admin/common/biz/user/UserFlowRIskControlInterceptor.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/common/biz/user/UserFlowRIskControlInterceptor.java
@@ -1,20 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.nageoffer.shortlink.admin.common.biz.user;
 
 import com.alibaba.fastjson2.JSON;
@@ -22,43 +5,44 @@ import com.google.common.collect.Lists;
 import com.nageoffer.shortlink.admin.common.convention.exception.ClientException;
 import com.nageoffer.shortlink.admin.common.convention.result.Results;
 import com.nageoffer.shortlink.admin.config.UserFlowRiskControlConfiguration;
-import jakarta.servlet.Filter;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.scripting.support.ResourceScriptSource;
+import org.springframework.web.servlet.HandlerInterceptor;
 
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Optional;
 
 import static com.nageoffer.shortlink.admin.common.convention.errorcode.BaseErrorCode.FLOW_LIMIT_ERROR;
 
 /**
- * 用户操作流量风控过滤器
- * 公众号：马丁玩编程，回复：加群，添加马哥微信（备注：link）获取项目资料
+ * ClassName: UserFlowRIskControlInterceptor
+ * Package: com.nageoffer.shortlink.admin.common.biz.user
+ * Description:
+ *
+ * @Author: Howie-New
+ * @Create: 2024/12/16 - 14:08
  */
-@Deprecated
+
+/**
+ * 用户操作流量风控拦截器
+ */
 @Slf4j
 @RequiredArgsConstructor
-public class UserFlowRiskControlFilter implements Filter {
+public class UserFlowRIskControlInterceptor implements HandlerInterceptor {
 
     private final StringRedisTemplate stringRedisTemplate;
     private final UserFlowRiskControlConfiguration userFlowRiskControlConfiguration;
 
     private static final String USER_FLOW_RISK_CONTROL_LUA_SCRIPT_PATH = "lua/user_flow_risk_control.lua";
 
-    @SneakyThrows
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         DefaultRedisScript<Long> redisScript = new DefaultRedisScript<>();
         redisScript.setScriptSource(new ResourceScriptSource(new ClassPathResource(USER_FLOW_RISK_CONTROL_LUA_SCRIPT_PATH)));
         redisScript.setResultType(Long.class);
@@ -68,14 +52,14 @@ public class UserFlowRiskControlFilter implements Filter {
             result = stringRedisTemplate.execute(redisScript, Lists.newArrayList(username), userFlowRiskControlConfiguration.getTimeWindow());
         } catch (Throwable ex) {
             log.error("执行用户请求流量限制LUA脚本出错", ex);
-            returnJson((HttpServletResponse) response, JSON.toJSONString(Results.failure(new ClientException(FLOW_LIMIT_ERROR))));
-            return;
+            returnJson(response, JSON.toJSONString(Results.failure(new ClientException(FLOW_LIMIT_ERROR))));
+            return false;
         }
         if (result == null || result > userFlowRiskControlConfiguration.getMaxAccessCount()) {
-            returnJson((HttpServletResponse) response, JSON.toJSONString(Results.failure(new ClientException(FLOW_LIMIT_ERROR))));
-            return;
+            returnJson(response, JSON.toJSONString(Results.failure(new ClientException(FLOW_LIMIT_ERROR))));
+            return false;
         }
-        filterChain.doFilter(request, response);
+        return true;
     }
 
     private void returnJson(HttpServletResponse response, String json) throws Exception {
--- admin/src/main/java/com/nageoffer/shortlink/admin/config/UserConfiguration.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/config/UserConfiguration.java
@@ -19,43 +19,60 @@ package com.nageoffer.shortlink.admin.config;
 
 import com.nageoffer.shortlink.admin.common.biz.user.UserFlowRiskControlFilter;
 import com.nageoffer.shortlink.admin.common.biz.user.UserTransmitFilter;
+import com.nageoffer.shortlink.admin.common.biz.user.UserTransmitInterceptor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * 用户配置自动装配
  * 公众号：马丁玩编程，回复：加群，添加马哥微信（备注：link）获取项目资料
  */
 @Configuration
-public class UserConfiguration {
+public class UserConfiguration implements WebMvcConfigurer {
+
 
     /**
-     * 用户信息传递过滤器
+     * 用户信息传递拦截器
+     * 用户操作流量风控拦截器
      */
-    @Bean
-    public FilterRegistrationBean<UserTransmitFilter> globalUserTransmitFilter() {
-        FilterRegistrationBean<UserTransmitFilter> registration = new FilterRegistrationBean<>();
-        registration.setFilter(new UserTransmitFilter());
-        registration.addUrlPatterns("/*");
-        registration.setOrder(0);
-        return registration;
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new UserTransmitInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/api/short-link/admin/v1/user/*");
+        registry.addInterceptor(new UserTransmitInterceptor())
+                .addPathPatterns("/**");
     }
 
-    /**
-     * 用户操作流量风控过滤器
-     */
-    @Bean
-    @ConditionalOnProperty(name = "short-link.flow-limit.enable", havingValue = "true")
-    public FilterRegistrationBean<UserFlowRiskControlFilter> globalUserFlowRiskControlFilter(
-            StringRedisTemplate stringRedisTemplate,
-            UserFlowRiskControlConfiguration userFlowRiskControlConfiguration) {
-        FilterRegistrationBean<UserFlowRiskControlFilter> registration = new FilterRegistrationBean<>();
-        registration.setFilter(new UserFlowRiskControlFilter(stringRedisTemplate, userFlowRiskControlConfiguration));
-        registration.addUrlPatterns("/*");
-        registration.setOrder(10);
-        return registration;
-    }
+//    /**
+//     * 用户信息传递过滤器
+//     */
+//    @Bean
+//    public FilterRegistrationBean<UserTransmitFilter> globalUserTransmitFilter() {
+//        FilterRegistrationBean<UserTransmitFilter> registration = new FilterRegistrationBean<>();
+//        registration.setFilter(new UserTransmitFilter());
+//        registration.addUrlPatterns("/*");
+//        registration.setOrder(0);
+//        return registration;
+//    }
+//
+//    /**
+//     * 用户操作流量风控过滤器
+//     */
+//    @Bean
+//    @ConditionalOnProperty(name = "short-link.flow-limit.enable", havingValue = "true")
+//    public FilterRegistrationBean<UserFlowRiskControlFilter> globalUserFlowRiskControlFilter(
+//            StringRedisTemplate stringRedisTemplate,
+//            UserFlowRiskControlConfiguration userFlowRiskControlConfiguration) {
+//        FilterRegistrationBean<UserFlowRiskControlFilter> registration = new FilterRegistrationBean<>();
+//        registration.setFilter(new UserFlowRiskControlFilter(stringRedisTemplate, userFlowRiskControlConfiguration));
+//        registration.addUrlPatterns("/*");
+//        registration.setOrder(10);
+//        return registration;
+//    }
 }
